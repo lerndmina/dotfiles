b9018b712de5421d4257e5919805c786bd47f959
Wild <wild@lerndmina.dev>
2024-05-01 01:12:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ function cloneAndStow() {
 
 # Ask if you want to install your public key
 read -p "Do you want to install your public key? (y/n) " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [[ $REPLY =~ "^[Yy]$" ]]; then
   # Check if .ssh directory exists, if not, create it
   if [ ! -d "$HOME/.ssh" ]; then
     mkdir $HOME/.ssh
