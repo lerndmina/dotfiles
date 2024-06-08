0136f22382c9da23f299e3ad9da07a0b8f692540
Adam <wild@lerndmina.dev>
2024-06-08 12:29:24 +0100

    Update initial-server-setup.sh

--- Scripts/initial-server-setup.sh
+++ Scripts/initial-server-setup.sh
@@ -5,22 +5,64 @@ if grep -q 'Ubuntu' /etc/os-release; then
   # Update the package list
   sudo apt-get update
 
-  # Install the necessary packages
-  sudo apt-get install -y fail2ban
 
-  # Setup fail2ban to protect against SSH attacks
-  sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+  THINGS_TO_INSTALL=()
 
-  # Configure fail2ban to ban IPs after 3 failed attempts for 10 minutes
-  sudo sed -i 's/maxretry = 5/maxretry = 3/g' /etc/fail2ban/jail.local
-  sudo sed -i 's/bantime  = 10m/bantime  = 10m/g' /etc/fail2ban/jail.local
+  # Ask the user if they want to install ufw
+  read -p "Do you want to install and configure ufw? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    THINGS_TO_INSTALL+=("ufw")
+  fi
 
-  # Start and enable fail2ban
-  sudo systemctl start fail2ban
-  sudo systemctl enable fail2ban
+  # Ask the user if they want to install fail2ban
+  read -p "Do you want to install and configure fail2ban? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    THINGS_TO_INSTALL+=("fail2ban")
+  fi
 
-  # Disable needrestart prompt
-  echo "\$nrconf{kernelhints} = -1;" > /etc/needrestart/conf.d/99disable-prompt.conf
+  # Install the packages
+  for package in "${THINGS_TO_INSTALL[@]}"; do
+    sudo apt-get install -y $package
+  done
+
+  # Enable the ufw firewall if it was installed
+  if [[ " ${THINGS_TO_INSTALL[@]} " =~ "ufw" ]]; then
+    # Enable the firewall
+    sudo ufw enable
+
+    # Allow SSH connections through the firewall
+    sudo ufw allow OpenSSH
+
+    # Allow HTTPS traffic
+    sudo ufw allow https
+
+    # Reload the firewall
+    sudo ufw reload
+  fi
+
+  # Enable the fail2ban service if it was installed
+  if [[ " ${THINGS_TO_INSTALL[@]} " =~ "fail2ban" ]]; then
+    # Install the necessary packages
+
+    # Setup fail2ban to protect against SSH attacks
+    sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+    # Configure fail2ban to ban IPs after 3 failed attempts for 10 minutes
+    sudo sed -i 's/maxretry = 5/maxretry = 3/g' /etc/fail2ban/jail.local
+    sudo sed -i 's/bantime  = 10m/bantime  = 10m/g' /etc/fail2ban/jail.local
+
+    # Start and enable fail2ban
+    sudo systemctl start fail2ban
+    sudo systemctl enable fail2ban
+
+    # Disable needrestart prompt
+    echo "\$nrconf{kernelhints} = -1;" > /etc/needrestart/conf.d/99disable-prompt.conf
+
+    sudo systemctl enable fail2ban
+    sudo systemctl start fail2ban
+  fi
 fi
 
 # Disable password authentication for the root account
@@ -31,8 +73,8 @@ sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/
 sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 sudo sed -i 's/PubkeyAuthentication no/#PubkeyAuthentication no/g' /etc/ssh/sshd_config
 
-# Disable root login
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
+# Disable root login with password allow root login with key
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin without-password/g' /etc/ssh/sshd_config
 sudo sed -i 's/PermitRootLogin yes/#PermitRootLogin yes/g' /etc/ssh/sshd_config
 
 # Restart the SSH service to apply the changes
